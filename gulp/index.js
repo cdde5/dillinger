7463a94a6d7a3f97d52c9a0c2c12c1f9660991e5
cdde5 <152792666+cdde5@users.noreply.github.com>
2024-07-01 16:07:54 +0800

    Update index.js

--- gulp/index.js
+++ gulp/index.js
@@ -1,13 +1,24 @@
 
-'use strict'
+'use strict';
 
-const fs = require('fs')
-const argv = require('yargs').argv
-const onlyScripts = require('./util/scriptFilter')
-const tasks = fs.readdirSync('./gulp/tasks/').filter(onlyScripts)
+const gulp = require('gulp');
+const fs = require('fs');
+const path = require('path');
 
-global.isProduction = !!(argv.production || argv.prod)
+// 获取 tasks 目录下的所有 .js 文件
+const taskFiles = fs.readdirSync(path.join(__dirname, 'tasks'))
+  .filter(filename => filename.endsWith('.js'));
 
-tasks.forEach(function (task) {
-  require('./tasks/' + task)
-})
+// 动态导入所有任务文件
+taskFiles.forEach(filename => {
+  const taskName = path.basename(filename, '.js');
+  const taskModule = require(`./tasks/${filename}`);
+  
+  // 如果任务模块导出了一个函数，将其注册为 Gulp 任务
+  if (typeof taskModule === 'function') {
+    gulp.task(taskName, taskModule);
+  }
+});
+
+// 导出 gulp 对象，以便其他文件可以使用
+module.exports = gulp;
