f8ed59d0130e2419a80781e251bf2b7f3a79f25c
cdde5 <152792666+cdde5@users.noreply.github.com>
2024-07-01 15:59:08 +0800

    Update default.js

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,6 +1,9 @@
 
 'use strict'
-
+// 导入其他任务
+require('./build');
+require('./critical');
 const gulp = require('gulp')
 
-gulp.task('default', ['build'])
+// 定义默认任务
+gulp.task('default', gulp.series('build', 'critical', /* 其他任务 */));
