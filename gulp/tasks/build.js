3073ecbcdd70d632c3a6db0a5901cf257e33bd12
cdde5 <152792666+cdde5@users.noreply.github.com>
2024-07-14 16:53:59 +0800

    Update build.js

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,23 +1,23 @@
-
-'use strict'
+'use strict';
 console.log('Starting Gulp build task');
-const gulp = require('gulp')
+const gulp = require('gulp');
 
-const sequence = require('run-sequence')
+const sequence = require('run-sequence');
 
-const devTasks = ['webpack:dev', 'sass']
+const devTasks = ['webpack:dev', 'sass'];
 
-const buildTasks = ['webpack:build', 'sass']
+const buildTasks = ['webpack:build', 'sass'];
 
 if (global.isProduction) {
   gulp.task('build', function () {
-    return sequence(buildTasks)
-  })
+    return sequence(buildTasks);
+  });
 } else {
-  gulp.task('build', devTasks)
+  // 修改 else 分支的代码
+  gulp.task('build', function() { 
+    return sequence(devTasks); 
+  });
 }
-
-
 
 // 你的任务代码
 console.log('Gulp build task completed');
